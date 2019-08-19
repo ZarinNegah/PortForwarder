476dffdb34fedba20c51e532a0e1fbbbfac9ce80
ZarinNegah <39865873+ZarinNegah@users.noreply.github.com>
2019-08-19 15:07:47 +0430

    Update and rename centos.sh to ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,16 +12,17 @@ echo "  Telegram: https://t.me/MTProtoConfiger"
 echo "---------------------------------------------"
 echo ""
 
-sudo yum update -y
-sudo yum upgrade -y
-sudo yum install tmux -y
-sudo yum -y install screen
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install wget -y
+sudo apt install nano -y
+sudo apt install tmux -y
+sudo apt -y install screen
 sudo wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
-sudo yum install golang-bin -y
+sudo apt install golang -y
 sudo wget -N --no-check-certificate https://raw.githubusercontent.com/ZarinNegah/PortForwarder/master/main.go
 sudo wget -N --no-check-certificate https://raw.githubusercontent.com/ZarinNegah/PortForwarder/master/rules.json
 sudo go build main.go
-sudo tmux
 if [ -z "$TMUX" ]; then
     ./main
 fi 

